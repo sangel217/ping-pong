22c51631614a91ec01084c76fe7a6ba5d3935bd6
sangel217 <30783953+sangel217@users.noreply.github.com>
2019-03-11 18:16:27 -0700

    Update scripts.js
    add code to the reflect for loop to append the list numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,29 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#number").val());
-    var result = counter(number);
+    var pingPongResult = counter(number);
+    for (var i = 0; i < number; i +=1){
+      $("ul#result").append("<li>" + number + "</li>");
+    
+    
 
 
-    $("#result").text(result);
+    $("#result").show();
 
   });
 });
 
 var counter = function(number){
-  if (number === 0 || number === 1)
-    return 1;
-  for (var i = 0; i <= number.length; i += 1) {
-    return i;
-  }
-  return true
+  var resultArray = [];
+  for (var i = 1; i <= number; i ++) {
+    if(i % 15) === 0) {
+      resultArray.push("pingpong");
+  } else if(i % 5 === 0) {
+    resultArray.push("pong");
+  } else if(i % 3 === 0) {
+    resultArray.push("ping");
+  } else { 
+    resultArray.push(i);
+  } 
+    return resultArray;
 };
